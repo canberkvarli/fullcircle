54e5cbe5f28eae703700d7b3f862d6f6742d0d16
Canberk Varli <canberkvarli@gmail.com>
2024-07-15 22:59:11 -0700

    install icons and style

--- app/onboarding/PhoneNumberScreen.tsx
+++ app/onboarding/PhoneNumberScreen.tsx
@@ -1,16 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import {
-  Button,
   StyleSheet,
   Alert,
   SafeAreaView,
   Text,
   View,
   TextInput,
+  TouchableOpacity,
 } from "react-native";
 import { useRouter } from "expo-router";
 import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
 import PhoneInput from "react-native-phone-number-input";
+import Icon from "react-native-vector-icons/FontAwesome"; // Import FontAwesome from react-native-vector-icons
 
 function PhoneNumberScreen(): JSX.Element {
   const [phoneNumber, setPhoneNumber] = useState("");
@@ -48,38 +49,56 @@ function PhoneNumberScreen(): JSX.Element {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Can we get your number?</Text>
-      <View style={styles.phoneContainer}>
-        <View style={styles.countryInputContainer}>
-          <PhoneInput
-            defaultValue={phoneNumber}
-            defaultCode="US"
-            layout="first"
-            onChangeText={(text) => {
-              setPhoneNumber(text);
-            }}
-            onChangeFormattedText={(text) => {
-              setFormattedPhoneNumber(text);
-            }}
-            containerStyle={styles.phoneInput}
-            textContainerStyle={styles.textContainer}
-            codeTextStyle={styles.codeText}
+      <View style={styles.topBar}>
+        <TouchableOpacity
+          style={styles.backButton}
+          onPress={() => router.replace("onboarding/LoginSignupScreen")}
+        >
+          <Icon name="chevron-left" size={24} color="black" />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.content}>
+        <Text style={styles.title}>Your Journey Begins Here</Text>
+        <Text style={styles.subtitle}>Enter your phone number to start.</Text>
+        <View style={styles.phoneContainer}>
+          <View style={styles.countryInputContainer}>
+            <PhoneInput
+              defaultValue={phoneNumber}
+              defaultCode="US"
+              layout="first"
+              onChangeText={(text) => {
+                setPhoneNumber(text);
+              }}
+              onChangeFormattedText={(text) => {
+                setFormattedPhoneNumber(text);
+              }}
+              containerStyle={styles.phoneInput}
+              textContainerStyle={styles.textContainer}
+              codeTextStyle={styles.codeText}
+            />
+          </View>
+          <TextInput
+            style={styles.phoneNumberInput}
+            placeholder="Phone Number"
+            value={phoneNumber}
+            onChangeText={setPhoneNumber}
+            keyboardType="phone-pad"
+            autoFocus={true}
           />
         </View>
-        <TextInput
-          style={styles.phoneNumberInput}
-          placeholder="Phone Number"
-          value={phoneNumber}
-          onChangeText={setPhoneNumber}
-          keyboardType="phone-pad"
-          autoFocus={true}
-        />
+        <Text style={styles.notificationText}>
+          We’ll text you a code to verify you’re really you. Message and data
+          rates may apply.
+        </Text>
+        <Text style={styles.affirmation}>
+          Step into a community of kindred spirits.
+        </Text>
       </View>
-      <Button title="Submit" onPress={handleSubmit} />
-      <Button
-        title="Back"
-        onPress={() => router.replace("onboarding/LoginSignupScreen")}
-      />
+      <View style={styles.bottomBar}>
+        <TouchableOpacity style={styles.nextButton} onPress={handleSubmit}>
+          <Icon name="chevron-right" size={24} color="black" />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
@@ -87,9 +106,24 @@ function PhoneNumberScreen(): JSX.Element {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  topBar: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    padding: 16,
+  },
+  content: {
+    flex: 1,
     padding: 16,
     justifyContent: "center",
     alignItems: "center",
+  },
+  bottomBar: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    padding: 16,
   },
   phoneContainer: {
     flexDirection: "row",
@@ -124,6 +158,30 @@ const styles = StyleSheet.create({
     fontSize: 24,
     marginBottom: 16,
     textAlign: "center",
+  },
+  subtitle: {
+    fontSize: 16,
+    marginBottom: 16,
+    textAlign: "center",
+  },
+  notificationText: {
+    fontSize: 12,
+    marginBottom: 16,
+    textAlign: "center",
+    color: "gray",
+  },
+  affirmation: {
+    fontSize: 16,
+    textAlign: "center",
+    fontStyle: "italic",
+  },
+  nextButton: {
+    padding: 12,
+    borderRadius: 30,
+  },
+  backButton: {
+    marginTop: 20,
+    marginLeft: 16,
   },
 });
 
