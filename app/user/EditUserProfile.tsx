e46d7aadebcec1e014c37475f2ad579f6651468a
Canberk Varli <canberkvarli@gmail.com>
2024-08-10 11:18:54 -0700

    add field titles and data. more to come

--- app/user/EditUserProfile.tsx
+++ app/user/EditUserProfile.tsx
@@ -41,7 +41,10 @@ export default function EditUserProfile() {
   };
 
   const handleFieldPress = (fieldName: string) => {
-    router.push("/user/EditFieldScreen", { fieldName });
+    router.push({
+      pathname: "/user/EditFieldScreen" as any,
+      params: { fieldName },
+    });
   };
 
   const renderItem = (item: { key: string; uri: string }, order: number) => {
@@ -94,6 +97,70 @@ export default function EditUserProfile() {
     uri,
   }));
 
+  const IdentityFields = [
+    {
+      fieldName: "Gender",
+      value: userData.gender,
+      isVisible: !userData.hiddenFields?.gender,
+    },
+    {
+      fieldName: "Sexuality",
+      value: userData.sexualOrientation,
+      isVisible: !userData.hiddenFields?.sexualOrientation,
+    },
+    {
+      fieldName: "I'm Interested In",
+      value: userData.datePreferences,
+      isVisible: !userData.hiddenFields?.datePreferences,
+    },
+  ];
+
+  const MyVirtuesFields = [
+    {
+      fieldName: "Work",
+      value: userData.jobLocation,
+      isVisible: !userData.hiddenFields?.jobLocation,
+    },
+    {
+      fieldName: "Job Title",
+      value: userData.jobTitle,
+      isVisible: !userData.hiddenFields?.jobTitle,
+    },
+    {
+      fieldName: "Education Level",
+      value: userData.educationDegree,
+      isVisible: !userData.hiddenFields?.educationDegree,
+    },
+  ];
+
+  // Additional user info for sections
+  const vitalFields = [
+    {
+      fieldName: "Name",
+      value: `${userData.firstName} ${userData.lastName}`,
+    },
+    {
+      fieldName: "Age",
+      value: userData.age,
+    },
+    {
+      fieldName: "Height",
+      value: userData.height,
+    },
+    {
+      fieldName: "Location",
+      value: userData.location?.city,
+    },
+    {
+      fieldName: "Ethnicities",
+      value: userData.ethnicities,
+    },
+    {
+      fieldName: "Children Preference",
+      value: userData.childrenPreference,
+    },
+  ];
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
@@ -145,47 +212,64 @@ export default function EditUserProfile() {
               />
             </View>
             <View style={styles.fieldsContainer}>
+              {/* IdentityFields Section */}
+              <Text style={styles.mainTitle}>Identity</Text>
+              <View style={styles.separator} />
+              {IdentityFields.map((field) =>
+                field.isVisible ? (
+                  <UserField
+                    key={field.fieldName}
+                    title={field.title}
+                    fieldName={field.fieldName}
+                    value={field.value}
+                    isVisible={field.isVisible}
+                    onPress={() => handleFieldPress(field.fieldName)}
+                  />
+                ) : null
+              )}
+            </View>
+            <View style={styles.fieldsContainer}>
+              {/* My Virtues Section */}
+              <Text style={styles.mainTitle}>My Virtues</Text>
+              <View style={styles.separator} />
+              {MyVirtuesFields.map((field) =>
+                field.isVisible ? (
+                  <UserField
+                    key={field.fieldName}
+                    title={field.title}
+                    fieldName={field.fieldName}
+                    value={field.value}
+                    isVisible={field.isVisible}
+                    onPress={() => handleFieldPress(field.fieldName)}
+                  />
+                ) : null
+              )}
+            </View>
+            <View style={styles.fieldsContainer}>
+              {/* My Vitals Section */}
+              <Text style={styles.mainTitle}>My Vitals</Text>
+              <View style={styles.separator} />
+              {vitalFields.map((field, index) => (
+                <UserField
+                  key={index}
+                  title={field.title}
+                  fieldName={field.fieldName}
+                  value={field.value}
+                  isVisible={true}
+                  onPress={() => handleFieldPress(field.title)}
+                />
+              ))}
+            </View>
+            <View style={styles.fieldsContainer}>
+              {/* My Vices Section */}
+              <Text style={styles.mainTitle}>My Vices</Text>
+              <View style={styles.separator} />
               <UserField
-                title="Identity"
-                fieldName="Gender"
-                value={userData.gender}
-                isVisible={!userData.hiddenFields.gender}
-                onPress={() => handleFieldPress("gender")}
-              />
-              <UserField
-                title="Sexual Orientation"
-                fieldName="Sexuality"
-                value={userData.sexualOrientation}
-                isVisible={!userData.hiddenFields.sexualOrientation}
-                onPress={() => handleFieldPress("sexualOrientation")}
-              />
-              <UserField
-                title="I'm Interested In"
-                fieldName="Date Preferences"
-                value={userData.datePreferences}
-                isVisible={!userData.hiddenFields.datePreferences}
-                onPress={() => handleFieldPress("datePreferences")}
-              />
-              <UserField
-                title="My Virtues"
-                fieldName="Work"
-                value={userData.jobLocation}
-                isVisible={!userData.hiddenFields.jobLocation}
-                onPress={() => handleFieldPress("jobLocation")}
-              />
-              <UserField
-                title="Job Title"
-                fieldName="Job Title"
-                value={userData.jobTitle}
-                isVisible={!userData.hiddenFields.jobTitle}
-                onPress={() => handleFieldPress("jobTitle")}
-              />
-              <UserField
-                title="Education Level"
-                fieldName="Education"
-                value={userData.educationDegree}
-                isVisible={!userData.hiddenFields.educationDegree}
-                onPress={() => handleFieldPress("educationDegree")}
+                title="Spiritual Practices"
+                fieldName="Spirituality"
+                value={userData.spiritualPractices}
+                isVisible={true}
+                onPress={() => handleFieldPress("Spirituality")}
               />
             </View>
           </View>
@@ -269,6 +353,16 @@ const styles = StyleSheet.create({
   },
   fieldsContainer: {
     marginTop: 16,
+  },
+  mainTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "gray",
+  },
+  separator: {
+    height: 1,
+    backgroundColor: "lightgray",
+    marginVertical: 8,
   },
   contentContainer: {
     flex: 1,
