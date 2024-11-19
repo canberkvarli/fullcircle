970477fa4cfe8750b03c7ac088ff39c2883bcd62
Canberk Varli <canberkvarli@gmail.com>
2024-11-18 20:50:39 -0800

    udpate age

--- app/main/Connect.tsx
+++ app/main/Connect.tsx
@@ -57,12 +57,12 @@ const ConnectScreen: React.FC = () => {
     if (userData) {
       setAgeRange([
         userData.filterOptions?.preferredAgeRange.min || 18,
-        userData.filterOptions?.preferredAgeRange.max || 35,
+        userData.filterOptions?.preferredAgeRange.max || 50,
       ]);
 
       setOriginalAgeRange([
         userData.filterOptions?.preferredAgeRange.min || 18,
-        userData.filterOptions?.preferredAgeRange.max || 35,
+        userData.filterOptions?.preferredAgeRange.max || 50,
       ]);
 
       setDatePreferences(userData.filterOptions?.datePreferences || []);
